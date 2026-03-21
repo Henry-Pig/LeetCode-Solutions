1677358140ce501e661e57e963978345f6d17ae5
Henry-Pig <61922893+Henry-Pig@users.noreply.github.com>
2026-03-21 18:30:34 +0800

    Time: 0 ms (100%), Space: 8.3 MB (53.36%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]<='Z'&&s[i]>='A')
+            {
+                s[i]+=32;
+            }
+        }
+        return s;
+    }
+};
